fd0750f03a531607f37fae5a4464c81bba667ac6
Rodrigo López <37187963+rodrigofez@users.noreply.github.com>
2023-06-10 17:05:46 -0600

    refactor: update contacts

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -7,7 +7,6 @@ import styles from "../styles/Contact.module.css";
 const contactList = [
   { title: "Github", src: "https://github.com/rodrigofez", icon: BrandGithub },
   { title: "Linkedin", src: "https://www.linkedin.com/in/rodrigolopezdev/", icon: BrandLinkedin },
-  { title: "Phone", src: "", icon: Phone },
   { title: "Email", src: "mailto:rodrigolp0521@gmail.com", icon: Mail },
 ];
 
@@ -18,9 +17,11 @@ export const Contact = () => {
     <Section id="contact" title={t("title")} subtitle={t("subtitle")}>
       <div className={styles.container}>
         {contactList.map((contact) => (
+          <a href={contact.src}>
           <div className={styles.card} key={contact.title}>
             <contact.icon size={28} strokeWidth="1.5px"></contact.icon>
           </div>
+          </a>
         ))}
       </div>
     </Section>
